866dcee283e97a33e8d923b1fe1aaaa47198c15c
derElektrobesen <pberejnoy2005@gmail.com>
2013-09-14 18:41:20 +0400

    Something changed;

--- network.c
+++ network.c
@@ -2,67 +2,73 @@
 
 int create_socket() {
     int sock = -1, listener = -1;
-	struct sockaddr_in addr;
-	char buf[1024];
-	int bytes_read;
-	fd_set set;
-	struct timeval tv;
+    struct sockaddr_in addr;
+    char buf[1024];
+    int bytes_read;
+    fd_set set;
+    struct timeval tv;
      
-	tv.tv_sec = 10000;
-	tv.tv_usec = 0; 
+    tv.tv_sec = 10000;
+    tv.tv_usec = 0; 
 
-	if ((listener = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("socket failure!");
-		exit(1);
+    if ((listener = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failure!");
+        exit(1);
     }
-	printf("\nlistener = %d\n", listener);
+    printf("\nlistener = %d\n", listener);
 
     addr.sin_family = AF_INET;
-	addr.sin_port = htons(PORT);
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
     
-	if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-		perror("bind failure!");
-		exit(1);
-	}
+    if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("bind failure!");
+        exit(1);
+    }
 
-	if (listen(listener, SELECT_QUEUE_LEN) < 0) {
-		perror("listen failure!");
-		exit(1);
-	}
+    if (listen(listener, SELECT_QUEUE_LEN) < 0) {
+        perror("listen failure!");
+        exit(1);
+    }
 
-	int err;
-	int flag;   
+    int err;
+    int flags;
+    int flag;
+    fcntl(listener, F_GETFL, &flags);
+    flags |= O_NONBLOCK;
+    fcntl(listener, F_SETFL, flags);
+    FD_ZERO(&set);    
+    FD_SET(listener, &set);
 
-	while(1) {
-		if ((err = select(listener + 1, &set, NULL, NULL, &tv)) > 0) {
-			if (FD_ISSET(listener, &set)) {
-				sock = accept(listener, NULL, NULL);
-				printf("\nsock = %d\n", sock);
-				if(sock < 0) {
-					perror("accept failure!");
-					exit(1);
-				}
-	
-				flag = 1; 
-				while(flag) { 
-					bytes_read = recv(sock, buf, 1024, 0);
-		  	
-					if(bytes_read <= 0) 
-						flag = 0;
-					else {
-						printf("Server received message: %s\n", buf);
-					}
-				}  
-	 			close(sock);
-			}
-		
-		} else if(err == 0) {
-	   		 printf("Time is up!\n");
-	   		 exit(1);
-		}
-	}
-	return 0;
+    while(1) {
+        if ((err = select(listener + 1, &set, NULL, NULL, &tv)) > 0) {
+            printf("Succesfull connection!\n");
+            if (FD_ISSET(listener, &set)) {
+                sock = accept(listener, NULL, NULL);
+                printf("\nsock = %d\n", sock);
+                if(sock < 0) {
+                    perror("accept failure!");
+                    exit(1);
+                }
+                flag = 1; 
+                while(flag) { 
+                    bytes_read = recv(sock, buf, 1024, 0);
+              
+                    if(bytes_read <= 0) 
+                        flag = 0;
+                    else {
+                        printf("Server received message: %s\n", buf);
+                    }
+                }  
+                 close(sock);
+            }
+        
+        } else if(err == 0) {
+                printf("Time is up!\n");
+                exit(1);
+        }
+    }
+    return 0;
 }
 
 int main() {
