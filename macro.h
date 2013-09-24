fee8f801fbbd6563af8fd033b890d7f7242865e1
derElektrobesen <pberejnoy2005@gmail.com>
2013-09-24 16:25:24 +0400

    Macro functions fixed

--- macro.h
+++ macro.h
@@ -3,11 +3,25 @@
 
 #include "config.h"
 
+/* For logging */
+#define SERVER 0
+#define CLIENT 1
+
 #ifdef DEBUG
-#   define log(...) \
-    printf(__VA_ARGS__)
-#else
+#   define print_cli(cli) \
+    (cli == SERVER ? "Server" : (cli == CLIENT ? "Client" : "Other"))
+
+#   define log(cli, f_str, args...) \
+        fprintf(stdout, "[ %s ] " f_str "\n", print_cli(cli), ##args)
+#   define err(cli, f_str, args...) \
+        fprintf(stderr, "[ %s ] " f_str "\n", print_cli(cli), ##args)
+#   define err_n(cli, f_str, args...) \
+        err(cli, f_str ": %s", strerror(errno), ##args) /* \n don't needed */
+
+#else /* DEBUG is undefined */
 #   define log(...)
-#endif
+#   define err(...)
+#   define err_n(...)
+#endif /* DEBUG */
 
 #endif
