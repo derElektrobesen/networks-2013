4da846e1be5455732513a4c5613a947e71772c70
Bakit Dzhumagulov <bakitds@gmail.com>
2013-10-12 19:22:26 +0400

    Proto updated

--- proto.c
+++ proto.c
@@ -1,14 +1,86 @@
 #include "proto.h"
 
-int encode_msg(struct proto_fields *fields, const char *msg, size_t msg_len) {
-    /* 1) Filling fields
-     * 2) Get hash algorithm
-     */
-    return -1;
+#define ITOC_MAX 12
+
+/* Функция преобразует целое положительное 
+ * число в строковое представление.
+ */
+char *itoc(int n) {
+    if (n < 0)
+        return NULL;
+    char *result = (char *)malloc(ITOC_MAX);
+    int i = 0;
+    do {
+        *(result + i) = n % 10 + '0';
+    } while((n/=10) > 0);
+    *(result + i) = 0;
+    return result;
+    /*without reverse*/
 }
 
-size_t decode_msg(const struct proto_fields *fields, char *msg) {
-    /* 1) Read fields 
+/* Функция создает ключ для хеширования.
+ */
+unsigned char *get_hash_key(struct proto_fields *fields) {
+    if (fields == NULL) {
+        err_n(SERVER, "incorrect protocol");
+        return NULL;
+    }
+    unsigned char *key1 = (unsigned char *)itoc(fields->pack_num);
+    unsigned char *key2 = 
+        (unsigned char *)itoc(fields->act_send_msg.piece_num);    
+    unsigned char *hash_key = malloc(sizeof(key1) + sizeof(key2) + 1);
+    hash_key = memcpy(hash_key, key1, sizeof(key1));
+    hash_key = memcpy(hash_key, key2, sizeof(key2));
+    free(key1);
+    free(key2);
+    return hash_key;
+}
+
+/* Функция создает хеш.
+ */
+unsigned char *get_hash(struct proto_fields *fields) {
+
+    if (fields == NULL) {
+        err_n(SERVER, "get_hash failure");
+        return NULL;
+    }
+    unsigned char *hash_key = get_hash_key(fields);
+    unsigned char *md5digest = malloc(MD5_DIGEST_LENGTH);
+    MD5_CTX md5handler;
+    MD5_Init(&md5handler);
+    MD5_Update(&md5handler, hash_key, sizeof(hash_key));
+    MD5_Final(md5digest, &md5handler);
+    
+    free(hash_key);
+    return md5digest;
+}
+
+/*
+ */
+int encode_msg(struct proto_fields *fields, char *msg) {
+    /* Fields filling */
+
+    fields->pack_num = 0;
+    fields->action_type = 0;
+    fields->act_send_msg.piece_num = 0;
+    fields->msg_len = strlen(msg); 
+
+    unsigned char *hash_msg = get_hash(fields);
+    if (hash_msg == NULL) {
+        err_n(SERVER, "get hash failure");
+        return -1;
+    }
+
+    /* Add serialization */
+
+
+
+    free(hash_msg);
+    return 0;
+}
+
+size_t decode_msg(const struct proto_fields *fields, const char *msg) {
+    /* 1) Read fields
      * 2) Decoding message
      * 3) return msg_len
      */
--- proto.h
+++ proto.h
@@ -1,7 +1,14 @@
 #ifndef PROTO_H
 #define PROTO_H
 
+#include <unistd.h>
+#include <errno.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/md5.h>
+#include "macro.h"
 
 #define ACT_SEND_MSG 1
 
@@ -10,14 +17,14 @@ struct proto_fields {
     int action_type;
     int msg_len;/* ? */
     union {
-        struct {
+    struct {
             int piece_num;
             int file_num;
         } act_send_msg;
     };
 };
 
-int encode_msg(struct proto_fields *fields, const char *msg, size_t msg_len);
-size_t decode_msg(const struct proto_fields *fields, char *msg);
+int encode_msg(struct proto_fields *fields, char *msn);
+size_t decode_msg(const struct proto_fields *fields, const char *msg);
 
 #endif
