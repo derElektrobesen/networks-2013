3421742f179a59b5fb39eff0910767d35b3d7816
derElektrobesen <pberejnoy2005@gmail.com>
2013-10-18 02:10:50 +0400

    dependence added

---
+++ types.h
@@ -0,0 +1,13 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <arpa/inet.h>
+
+/* Структура хранит массив активных сокетов */
+struct sockets_queue {
+    int sockets[MAX_CONNECTIONS];       /*< Массив сокетов              */
+    in_addr_t addrs[MAX_CONNECTIONS];   /*< Массив активных подключений */
+    int count;                          /*< Число акивных подключений   */
+};
+
+#endif
