2e52a97496505a3c2bf1dfd5807c420c9601b38f
Bakit Dzhumagulov <bakitds@gmail.com>
2013-10-19 18:32:58 +0400

    Proto Updated

--- proto.h
+++ proto.h
@@ -10,59 +10,34 @@
 #include <openssl/md5.h>
 #include "macro.h"
 
-#define ACT_DOWNLOAD_MSG        1
-#define ACT_DOWNLOAD_MSG_ANSW   2
-#define ACT_SEARCH_FILE         3
-#define ACT_SEARCH_FILE_ANSW    4
-
-#ifdef PROTO_DIVIDER
-#   define DIVIDER_LENGTH 1
-#else 
-#   define DIVIDER_LENGTH 0
-#endif
-
-
 typedef unsigned int pack_id_t;
 #define PACK_ID_LENGTH sizeof(pack_id_t)
 typedef unsigned int piece_num_t;
 #define PIECE_NUM_LENGTH sizeof(piece_num_t)
 typedef unsigned int file_num_t;
 #define FILE_NUM_LENGTH sizeof(file_num_t)
+typedef unsigned int piece_len_t;
+#define PIECE_LEN_TSIZE sizeof(piece_len_t)
 
-#define PROTO_ACTION_TSIZE 4
 
-struct proto_fields {
+struct cli_fields {
     pack_id_t pack_id;
-    unsigned int action_type;
-    unsigned int msg_len;
-    union {
-        /* Запрос клиента на скачивание файла */
-        struct {
-            piece_num_t piece_num;
-            file_num_t file_num;
-            unsigned char sum[MD5_DIGEST_LENGTH];
-        } act_download_piece;
-        /* Ответ сервера на запрос на скачивание файла */
-        struct {
-            unsigned char sum[MD5_DIGEST_LENGTH];
-            unsigned char data[BUF_MAX_LEN - 
-                               CONTROL_INFO_LEN - 
-                               MD5_DIGEST_LENGTH];
-        } act_download_piece_answ;
-        /* Запрос клиента на поиск файла */
-        struct {
-            char file_path[FILE_NAME_MAX_LEN];
-        } act_search_file;
-        /* Ответ сервера на запрос поиска файла */
-        struct {
-            unsigned char sum[MD5_DIGEST_LENGTH];
-            unsigned int file_num;
-            unsigned long file_size;
-        } act_search_file_answ;
-    };
+    piece_num_t piece_num;
+    int piece_len;
+    unsigned short error;
+    char file_name[FILE_NAME_MAX_LEN];
+    unsigned char hsumm[MD5_DIGEST_LENGTH];
 };
 
-int encode_msg(struct proto_fields *fields, const char *msg);
-size_t decode_msg(const struct proto_fields *fields, char *msg);
+struct srv_fields {
+    struct cli_fields;
+    unsigned char piece[BUF_MAX_LEN - sizeof(struct cli_fields)];
+};
+
+int encode_cli_msg(struct cli_fields *fields, const char *msg);
+size_t decode_cli_msg(const struct cli_fields *fields, char *msg);
+int encode_srv_msg(struct srv_fields *fields, const char *msg);
+size_t decode_srv_msg(const struct srv_fields *fields, char *msg);
+
 
 #endif
