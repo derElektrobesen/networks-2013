c7ccb337048f03463160179362299bcb68092bfb
derElektrobesen <pberejnoy2005@gmail.com>
2013-11-07 20:04:57 +0400

    srv fixed

--- srv.h
+++ srv.h
@@ -5,9 +5,18 @@
 
 #include "proto.h"
 
+struct file_cache {
+    unsigned char data[CACHED_PIECES_COUNT * DATA_BLOCK_LEN];
+    piece_id_t start_piece;
+    piece_id_t end_piece;
+    char name[FILE_NAME_MAX_LEN];
+    FILE *file;
+};
+
 struct files_queue {
-    char names[FILE_NAME_MAX_LEN][MAX_TRANSMISSIONS];
-    int cur;
+    char positions[MAX_TRANSMISSIONS]; /* 0 (free) & 1 (reserved) */
+    struct file_cache cache[MAX_TRANSMISSIONS];
+    int count;
 };
 
 /* Обрабатывает сообщения, поступающие от клиентов */
