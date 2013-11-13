369ae24c0e8833e33f2d9dbba9f9bb140a42d287
Bakit Dzhumagulov <bakitds@gmail.com>
2013-11-13 23:16:53 +0400

    Daemon added

--- main.c
+++ main.c
@@ -1,9 +1,96 @@
 #include "network.h"
+#include <unistd.h>
+#include <syslog.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
 #ifdef CLI
 #include <time.h>
 
 #   include "cli.h"
+
+/* Не знаю как перенести в Makefile */
+#define CLI_LOCK_FILE_NAME "/var/run/cli.pid"
+#define SRV_LOCK_FILE_NAME "/var/run/srv.pid"
+#define LOCK_MODE   (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+
+int lockfile(int fd)
+{
+    struct flock fl;
+    fl.l_type = F_WRLCK;
+    fl.l_start = 0;
+    fl.l_whence = SEEK_SET;
+    fl.l_len = 0;
+    return (fcntl(fd, F_SETLK, &fl));
+}
+
+int is_running(const char *file_name)
+{
+    int fd;
+    char buf[16];
+    fd = open(file_name, O_RDWR | O_CREAT, LOCK_MODE);
+    if (fd < 0) {
+        syslog(LOG_ERR, "could not open file %s", file_name);
+        exit(1);
+    }
+    if (lockfile(fd) < 0) {
+        if (errno == EACCES || errno == EAGAIN) {
+            close(fd);
+            return -1;
+        }
+        syslog(LOG_ERR, "could not open file %s", file_name);
+        exit(1);
+    }
+    ftruncate(fd, 0);
+    sprintf(buf, "%ld", (long)getpid());
+    write(fd, buf, strlen(buf) + 1);
+    return fd;
+}
+
+void daemonize(const char *cmd, const char *file_name)
+{
+    int i;
+    pid_t pid;
+    struct rlimit rl;
+    int block_fd;
+
+    if ((block_fd = is_running(file_name)) == -1) {
+        syslog(LOG_ERR, "%s is already running", cmd);
+        exit(1);
+    }
+
+    umask(0);
+    if (getrlimit(RLIMIT_NOFILE, &rl) < 0){
+        /* add log */
+        return;
+    }
+    
+    if ((pid = fork()) < 0) {
+        return;
+    } else if (pid != 0){
+        exit(0);
+    }
+    setsid();
+
+    if (chdir("/") < 0) {
+        /*add log*/
+        return;
+    }
+
+    if (rl.rlim_max == RLIM_INFINITY)
+        rl.rlim_max = 1024;
+    for (i = 0; i < rl.rlim_max && i != block_fd; i++)
+        close(i);
+   
+    openlog(cmd, LOG_CONS | LOG_PID, LOG_DAEMON);
+}
+
 
 struct sockets_queue q;
 
@@ -31,12 +118,14 @@ int main(int argc, char **argv) {
 #   include "srv.h"
 
 int main(int argc, char **argv) {
+	/*daemonize(argv[0], SRV_LOCK_FILE_NAME);*/
     return start_server(&process_client_message);
 }
 
 #else
 
 int main(int argc, char **argv) {
+	/*daemonize(argv[0], CLI_LOCK_FILE_NAME);*/
     err(-1, "Compile define option is required");
     return 1;
 }
