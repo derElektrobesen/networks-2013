b450d85688050d3961707c1f4803f2921c4aaf0c
derElektrobesen <pberejnoy2005@gmail.com>
2013-12-16 14:41:21 +0400

    Log saving implemented

--- frontend/otherwindows_d.py
+++ frontend/otherwindows_d.py
@@ -3,6 +3,8 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+from log import Logger
+
 from forms import FormTorrent, FormAbout, FormLogs
 
 class TorrentWindow(QDialog, FormTorrent):
@@ -18,10 +20,24 @@ class AboutWindow(QDialog, FormAbout):
         QDialog.__init__(self, parent)
         self.setupUi(self)
 
-class LogsWindow(QDialog, FormLogs):
+class LogsWindow(QMainWindow, FormLogs):
     def __init__(self, parent = None):
         QDialog.__init__(self, parent)
         self.setupUi(self)
 
     def set_logs(self, messages):
         self.log_edit.setPlainText(messages)
+
+    @pyqtSlot()
+    def on_save_report_act_triggered(self):
+        filename = QFileDialog.getSaveFileName(self, "Сохранить отчет", "HOME_PATH")
+        if not filename:
+            return
+        try:
+            with open(filename, 'w') as f:
+                Logger.log("Отчет был сохранен в файл '{name}'".format(name = filename))
+                f.write(Logger.get_log())
+        except IOError as e:
+            msg = "При сохранении отчета возникла ошибка: {text}".format(text = str(e.args[1]))
+            Logger.log(msg)
+            QMessageBox.critical(self, "Ошибка", msg)
